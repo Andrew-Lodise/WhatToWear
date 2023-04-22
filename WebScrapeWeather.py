4aaa590c17f40551aa95b1dd432cc6c3355d45aa
Andrew-Lodise <AL964440@wcupa.edu>
2023-04-21 21:31:03 -0400

    moved msg to wsw

--- WebScrapeWeather.py
+++ WebScrapeWeather.py
@@ -7,20 +7,24 @@ def get_weather(query):
     s = HTMLSession()
     url = f'https://www.google.com/search?q=weather+{query}'
     dict = {}
-
+    # idk what these headers are tbh
     r = s.get(url, headers={"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.48'})
     #print(r.html.find('title',first=True).text)
-    dict['temp'] = r.html.find('span#wob_tm',first=True).text
+    dict['temp'] = int(r.html.find('span#wob_tm',first=True).text)
     dict['unit'] = r.html.find('div.vk_bk.wob-unit span.wob_t', first=True).text # search for class with div
     dict['desc'] = r.html.find('div.VQF4g', first=True).find('span#wob_dc', first=True).text
-    dict['high'] = r.html.find('div.gNCp2e', first=True).find('span.wob_t', first=True).text
-    dict['low'] = r.html.find('div.wNE31c', first=True).find('span.wob_t')[2].text
+    dict['high'] = int(r.html.find('div.gNCp2e', first=True).find('span.wob_t', first=True).text)
+    dict['low'] = int(r.html.find('div.wNE31c', first=True).find('span.wob_t')[2].text)
     dict['percip'] = r.html.find('div.wtsRwe', first=True).find('span#wob_pp', first=True).text
     dict['humid'] = r.html.find('div.wtsRwe', first=True).find('span#wob_hm', first=True).text
     dict['wind'] = r.html.find('div.wtsRwe', first=True).find('span#wob_ws', first=True).text
+    #msg format looks weird but works and keeps it not 1 long line
+    dict['msg'] = f'''Today is {dict['temp']}{dict['unit']} and {dict['desc']}. 
+The high is {dict['high']}{dict['unit']} and the low is {dict['low']}{dict['unit']}.
+Percipitation:{dict['percip']} Humidity:{dict['humid']} Wind Speed:{dict['wind']}'''
     return dict
     #print(f"Current weather in {query}: {desc} {temp}{unit}")
     #print(f"High: {high} \t Low: {low}")
 
-
-#print(get_weather("Philadelphia"))
+dict = get_weather("Philadelphia")
+#print(dict['msg'])
--- main.py
+++ main.py
@@ -6,11 +6,8 @@ def main():
 
     city = "Philadelphia"
     wd = get_weather(city) # wd = weather data
-    msg = f'''Today is {wd['temp']}{wd['unit']} and {wd['desc']}.
-    \nThe high is {wd['high']}{wd['unit']} and the low is {wd['low']}{wd['unit']}.
-    \nPercipitation:{wd['percip']} Humidity:{wd['humid']} Wind Speed:{wd['wind']}'''
 
-    print(msg)
-    email_alert(msg)
+    print(wd['msg'])
+    #email_alert(msg)
 
 main()
